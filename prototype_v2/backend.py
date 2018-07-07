01b7e5de1c4fde63364739d137c0184ffb0d98e4
Nicolas Martin <nicolas.martin@stud.h-da.de>
2018-07-07 12:56:40 +0200

    backend aktualisiert

--- prototype_v2/backend.py
+++ prototype_v2/backend.py
@@ -19,10 +19,11 @@ import re
 #==============================================================================================================
 
 class Food:
-    def __init__(self, name, count, water):
+    def __init__(self, name, count, water, startCount):
         self.name = name
         self.count = count
         self.water = water
+        self.startCount = startCount
 
 #==============================================================================================================
 #=== Initialisierung ===
@@ -34,6 +35,8 @@ products = []
 totalWater = 0
 actualProduct= "nothing"
 jsonString = ""
+jsonData = []
+locked = False
 
 #=== SQL Connection ===
 sqlConn = pymysql.connect(host='127.0.0.1',
@@ -65,7 +68,9 @@ class InputReader(threading.Thread):
         global products
         global actualProduct
         global jsonString
-        jsonData = []
+
+        global jsonData
+
         while 1:
 
             #liest den arduino aus
@@ -78,21 +83,28 @@ class InputReader(threading.Thread):
                 actualInput = actualInput.decode("utf-8")
                 actualInput = str(actualInput)
 
+                #waehlt Eintrag aus der Datenbak aus
+
                 sql = "SELECT * FROM `food` WHERE rfid=\'" + actualInput + "\'"
                 cursor.execute(sql)
 
                 data = cursor.fetchone()
 
-                #verarbeitet den SQL Eintrag, und schreibt ihn in die beiden Arrays
+                #verarbeitet den SQL Eintrag, und schreibt ihn in das Products-Array
                 if data:
                     actualName = data[1]
                     if actualName not in productNameList:
                         productNameList.append(actualName)
-                        products.append(Food(data[1], data[7], data[6]))
+                        products.append(Food(data[1], data[7], data[6], data[7]))
                     
+                    #Setzt das zuletzt eingescannte Produkt als actualProduct
                     actualProduct = actualName
 
+                #Ueberfuehrt das products array in ein JSON Objekt
+                
+                jsonData = []
                 for product in products:
+                    product.count = int(product.count)
                     jsonData.append(json.dumps(product.__dict__))
                 
                 jsonString = json.dumps(jsonData)  
@@ -120,14 +132,99 @@ inputReaderThread.start()
 async def backendSocket(websocket, path):
 
 
+    global actualProduct
     global jsonString
+    global jsonData
+    global locked
+    global products
     while True:
         value = await websocket.recv()
 
-        if value == "getVariables":
-            await websocket.send(jsonString)
+        #Teilt den empfangenen String an den Leerzeichen auf, und schreibt ihn in ein Array
+        valueSplitted = value.split()
+        
+        if value == "unlockScann":
+            locked = False
+
+
+        elif value == "getActualProduct":
+            await websocket.send(json.dumps(actualProduct))
+        
+
+        elif value == "resetBackendActualProduct":
+            actualProduct = "nothing"
+
+
         elif value == "getTotalWater":
             await websocket.send(json.dumps(1324))
+
+
+        elif value == "getProducts":
+            await websocket.send(jsonString)
+
+
+        elif valueSplitted[0] == "add":
+
+            actualName = valueSplitted[1]
+            actualCount = int(valueSplitted[2])
+
+            if actualName not in productNameList:
+                
+                productNameList.append(actualName)
+
+                sql = "SELECT * FROM `food` WHERE name=\'" + actualName + "\'"
+                cursor.execute(sql)
+
+                data = cursor.fetchone()
+
+                #verarbeitet den SQL Eintrag, und schreibt ihn in das Products-Array
+                if data:
+                    products.append(Food(data[1], actualCount, data[6], data[7]))
+            
+            #erhoeht die Anzahl des jeweiligen Produkts um die angegebene Zahl
+
+            else:
+                for product in products:
+                    if actualName == product.name:
+                        product.count = product.count + actualCount
+                print(products[0].count)
+            
+            #Ueberfuehrt das products array in ein JSON Objekt
+            jsonData = []
+            for product in products:
+                product.count = int(product.count)
+                jsonData.append(json.dumps(product.__dict__))
+            
+            jsonString = json.dumps(jsonData)
+
+
+
+        elif valueSplitted[0] == "delete":
+
+            actualName = valueSplitted[1]
+            actualCount = int(valueSplitted[2])
+
+            for product in list(products):
+                if product.name == actualName:
+                    product.count = product.count - actualCount
+
+
+                
+                    #Ueberfuehrt das products array in ein JSON Objekt
+
+                    jsonData = []
+                    for product in products:
+                        product.count = int(product.count)
+                        jsonData.append(json.dumps(product.__dict__))
+                    
+                    jsonString = json.dumps(jsonData)
+        
+        elif value == "printBill":
+            print("printing Bill")
+
+        elif value == "resetBackend":
+            print("Backend wurde resettet")
+
         else:
             await websocket.send(value)
 
